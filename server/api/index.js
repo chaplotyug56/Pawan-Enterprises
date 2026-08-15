79237b2131a3a6871701c674618859524df33fc0
Yug Chaplot <chaplotyug56@gmail.com>
2026-08-15 20:31:40 +0530

    chore: clean up vercel entrypoint after fixing ESM bug

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,3 @@
-let app;
-try {
-  app = require("../server.js");
-} catch (err) {
-  app = (req, res) => {
-    res.statusCode = 500;
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify({ error: "Initialization Error", message: err.message, stack: err.stack }));
-  };
-}
+const app = require("../server.js");
 
 module.exports = app;
